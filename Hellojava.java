279a25540f48f9f0af51a308f19f8255e7a4301c
vasanth8072884324 <vasanth01818@gmail.com>
2024-10-09 13:23:46 +0530

    git add . git commit -m "Changed Fibonacci series to Armstrong Number program"

--- Hellojava.java
+++ Hellojava.java
@@ -1,5 +1,21 @@
-public class HelloJava {
+public class ArmstrongNumber {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        int number = 153; // Example number
+        int originalNumber = number;
+        int remainder;
+        int result = 0;
+
+        while (originalNumber != 0) {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3); // For three-digit numbers
+            originalNumber /= 10;
+        }
+
+        if (result == number) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
     }
 }
+
